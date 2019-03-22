773bbee73aa0adc6afe4dddccb7d51effe6019c4
act262 <act262@gmail.com>
2019-03-22 21:41:50 +0800

    feat(demo): Add sample use case

--- app/src/main/java/io/zcx/plugin/wrapper/bazel/MainActivity.java
+++ app/src/main/java/io/zcx/plugin/wrapper/bazel/MainActivity.java
@@ -2,6 +2,12 @@ package io.zcx.plugin.wrapper.bazel;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -11,8 +17,32 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"hello",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
 
         Toast.makeText(this, "world", Toast.LENGTH_SHORT).show();
+
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View inflate = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_type1, viewGroup, false);
+                //@formatter:off
+                return new RecyclerView.ViewHolder(inflate) {};
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+        });
     }
 }
